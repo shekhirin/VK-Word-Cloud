3084477bbb8df5fb3514c263bcfe5484fcbb1f40
elishanto <elishanto@gmail.com>
2017-01-01 19:25:19 +0300

    Minor bugfixes

--- vk_wc.py
+++ vk_wc.py
@@ -20,6 +20,8 @@ from multiprocessing.pool import Pool
 from tqdm import tqdm
 from pymongo import MongoClient
 import config
+import math
+from textblob import TextBlob as tb
 
 vk_group = vk_api.VkApi(token=config.vk_community_token).get_api()
 vk_session = vk_api.VkApi(token=config.vk_user_token)
@@ -29,6 +31,18 @@ collection = MongoClient()['wordcloud']['photos']
 remove_words = ['год']
 
 processing = []
+
+def tf(word, blob):
+    return blob.words.count(word) / len(blob.words)
+
+def n_containing(word, bloblist):
+    return sum(1 for blob in bloblist if word in blob.words)
+
+def idf(word, bloblist):
+    return math.log(len(bloblist) / (1 + n_containing(word, bloblist)))
+
+def tfidf(word, blob, bloblist):
+    return tf(word, blob) * idf(word, bloblist)
 
 def cloud(user_id):
     wall = []
@@ -57,21 +71,19 @@ def cloud(user_id):
         return
     def color_func(word, font_size, position, orientation, random_state=None, **kwargs):
         return "hsl(%d, 100%%, %d%%)" % (random.randint(0, 360), random.randint(20, 50))
-    sw = (stopwords.words('russian') + stopwords.words('english'))
+    sw = (stopwords.words('russian') + stopwords.words('english') + remove_words)
     wordcloud = WordCloud(
-        max_words=200,
-        max_font_size=300,
         background_color='white',
         margin=5,
-        stopwords=sw,
-        width=500,
-        height=500
+        width=1000,
+        height=1000,
+        stopwords=sw
     ).generate(' '.join(top_words))
     wordcloud = wordcloud.recolor(color_func=color_func, random_state=3)
     wordcloud.to_file('clouds/{}.jpg'.format(user_id))
-    return open('clouds/{}.jpg'.format(user_id), 'rb')
+    return open('clouds/{}.jpg'.format(user_id), 'rb'), top_words
 
-def make_cloud(user_id):
+def send_cloud(user_id):
     processing.append(user_id)
     try:
         if not vk.groups.isMember(group_id=config.group_id, user_id=user_id):
@@ -102,15 +114,14 @@ def make_cloud(user_id):
         name = user['first_name'] + ' ' + user['last_name']
         data = vk.photos.getUploadServer(album_id=config.album_id, group_id=config.group_id)
         DATA_UPLOAD_URL = data['upload_url']
-        clouded = cloud(user_id)
+        clouded, words = cloud(user_id)
         if not clouded:
             vk_group.messages.send(user_id=user_id, message='Похоже, у тебя недостаточно записей на стене для составления облака тегов☹️')
             time.sleep(5)
             return
         r = requests.post(DATA_UPLOAD_URL, files={'photo': clouded}).json()
         photo = vk.photos.save(server=r['server'], photos_list=r['photos_list'], group_id=r['gid'], album_id=r['aid'], hash=r['hash'])[0]
-        if not collection.find_one({'user_id': user_id}):
-            collection.insert({'user_id': user_id, 'owner_id': photo['owner_id'], 'id': photo['id']})
+        collection.insert({'user_id': user_id, 'owner_id': photo['owner_id'], 'id': photo['id']})
         # post = vk.wall.post(owner_id=-136503501, from_group=1, message='Облако тегов за 2016 год для *id{}({})'.format(user_id, name), attachments='photo{}_{}'.format(photo['owner_id'], photo['id']))
         vk_group.messages.send(user_id=user_id, message='А вот и твое облако тегов! 🌍', attachment='photo{}_{}'.format(photo['owner_id'], photo['id']))
         vk_group.messages.send(user_id=user_id, message='Не забудь рассказать друзьям 😉')
@@ -120,22 +131,31 @@ def make_cloud(user_id):
         pass
     processing.remove(user_id)
 
+def send_friend_cloud(user_id, friend_id=None):
+    if not friend_id:
+
+        time.sleep(5)
+
 def process_updates(updates):
     for update in updates:
-        if update[0] == 4 and update[3] not in processing and update[6].lower() == 'облако':
-            Thread(target=make_cloud, args=(update[3], )).start()
+        if update[0] == 4 and update[3] not in processing:
+            if update[6].lower() in 'облако':
+                Thread(target=send_cloud, args=(update[3], )).start()
+            elif update[6].lower() in 'облако для друга':
+                Thread(target=send_friend_cloud, args=(update[3], )).start()
 
-longpoll = vk_group.messages.getLongPollServer()
-while True:
-    try:
-        response = requests.get('https://{}?act=a_check&key={}&ts={}&wait=25%mode=128'.format(
-            longpoll['server'],
-            longpoll['key'],
-            longpoll['ts']
-        ), timeout=25).json()
-        longpoll['ts'] = response['ts'] if 'ts' in response else longpoll['ts']
-        Thread(target=process_updates, args=(response['updates'], )).start()
-    except Exception as e:
-        longpoll = vk_group.messages.getLongPollServer()
-        print(e)
-        continue
+if __name__ == '__main__':
+    longpoll = vk_group.messages.getLongPollServer()
+    while True:
+        try:
+            response = requests.get('https://{}?act=a_check&key={}&ts={}&wait=25%mode=128'.format(
+                longpoll['server'],
+                longpoll['key'],
+                longpoll['ts']
+            ), timeout=25).json()
+            longpoll['ts'] = response['ts'] if 'ts' in response else longpoll['ts']
+            Thread(target=process_updates, args=(response['updates'], )).start()
+        except Exception as e:
+            longpoll = vk_group.messages.getLongPollServer()
+            print(e)
+            continue
