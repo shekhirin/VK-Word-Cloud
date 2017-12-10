7e639566b74117a6048f49e59bea58f0cc2199e1
Alexey Shekhirin <elishanto@gmail.com>
2017-12-10 19:06:05 +0300

    Change config behaviour

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+
+vk_community_token = os.environ.get('vk_community_token')
+vk_user_token = os.environ.get('vk_user_token')
+album_id = os.environ.get('album_id')
+group_id = os.environ.get('group_id')
--- config_example.py
+++
@@ -1,4 +0,0 @@
-vk_community_token = 'COMMUNITY_TOKEN (access to messages)'
-vk_user_token = 'USER_TOKEN (scope = offline,photos)'
-album_id = 'ALBUM_FOR_CLOUDS'
-group_id = 'GROUP_OF_ALBUM'
